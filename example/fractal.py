e7367f861951069468ad14f4f5259bdb2edc3f15
fehlfarbe <marcus@degenkolbe.eu>
2020-12-21 03:40:23 +0100

    Updated fractal example:   - added camera param   - added new example image   - added pose estimation

--- example/fractal.py
+++ example/fractal.py
@@ -5,20 +5,25 @@ import numpy as np
 import aruco
 
 if __name__ == '__main__':
-
-    print(aruco.__dict__)
-
     # create fractaldetector and set config
     detector = aruco.FractalDetector()
-    print(detector.setConfiguration(0))
+    # set detector config first or it will throw an exception
+    detector.setConfiguration(0)
+    # load camera parameters
+    camparam = aruco.CameraParameters()
+    camparam.readFromXMLFile(filePath="rollei_live.yml")
+    # set camera parameters
+    if camparam.isValid():
+        detector.setParams(camparam, 0.17)
 
     # load test image
-    frame = cv2.imread(os.path.join(os.path.dirname(__file__), "fractal_test.jpg"))
+    frame = cv2.imread(os.path.join(os.path.dirname(__file__), "rollei_snap.jpg"))
 
     # detect markers
     detected = detector.detect(frame)
 
     if detected:
+        detector.drawMarkers(frame)
         markers = detector.getMarkers()
         # print id and points
         for marker in markers:
@@ -27,14 +32,22 @@ if __name__ == '__main__':
             for i, point in enumerate(marker):
                 print("\t{:d} {}".format(i, str(point)))
             marker.draw(frame, np.array([255, 255, 255]), 2)
+
         print("detected ids: {}".format(", ".join(str(m.id) for m in markers)))
         # draw fractal marker using detector
         detector.draw2d(frame)
+
+    # get Pose and draw axis/cube
+    if detector.poseEstimation():
+        tvec = detector.getTvec()
+        rvec = detector.getRvec()
+        print("TVec: {}\nR: {}".format(tvec, rvec))
+        # draw cube
+        detector.draw3d(frame)
 
     # show frame
     if 'DISPLAY' in os.environ.keys():
         cv2.imshow("frame", frame)
         k = cv2.waitKey(0)
-        print(k)
     else:
         print("No display!")
